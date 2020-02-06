7593818387c6b150760ecbdb09a07c4a43795d73
Laurent Morin <laurent.morin@irisa.fr>
2020-02-06 17:29:12 +0100

    Add management of font

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,54 @@
 
 #define HPASTEUR_DISPLAY_TESTS
 #include <HpasteurDisplay.h>
+#include <font.h>
 
+namespace hpasteur {
+  /*
+   */
+  class HPLetterFont : public HPLetter, public Font {
+  public:
+    /* Constructor */ HPLetterFont(void) : HPLetter(), Font() {}
+
+    bool write(char chr) {
+      uint8_t * charTable;
+      size_t char_size;
+      if (getCharacterByChar(chr, charTable, char_size)) {
+        for (size_t idx=0; idx < char_size; idx++)
+          ledOn(charTable[idx]);
+        return true;
+      }
+      return false;
+    }
+  };
+
+}
+    
 using namespace hpasteur;
 
 int main(void)
 {
-  HPLetter gpioSegment;
-  swapLeds(gpioSegment);
+  //HPLetter gpioSegment;
+  //swapLeds(gpioSegment);
+
+  HPLetterFont hpSegment;
+  while (1) {
+    for (uint8_t count = 0; count < 2; count++) {
+      for (uint8_t index = 0; index < hpSegment.ledCount(); index++) {
+        hpSegment.ledOn(index);
+      }
+      _delay_ms(1000);
+    }
+
+    for (char letter='A'; letter <= 'Z'; letter++) {
+      for (size_t index = 0; index < 100; index++) {
+        hpSegment.write(letter);
+      }
+    }
+    for (char digit='0'; digit <= '9'; digit++) {
+      for (size_t index = 0; index < 100; index++) {
+        hpSegment.write(digit);
+      }
+    }
+  }
 }
